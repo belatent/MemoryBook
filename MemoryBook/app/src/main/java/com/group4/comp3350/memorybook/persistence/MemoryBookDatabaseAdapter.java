c8be4a39250c7d720feb2980bc193f6958942b4e
rads17 <z-dar@live.com>
2016-02-02 13:59:11 -0600

    Created MemoryBookDatabaseInterface & MemoryBookDatabaseStub

--- MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/persistence/MemoryBookDatabaseAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.group4.comp3350.memorybook.persistence;
-
-/**
- * Created by Bennet on 2/2/2016.
- */
-public class MemoryBookDatabaseAdapter {
-}
--- MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/persistence/MemoryBookDatabaseInterface.java
+++ MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/persistence/MemoryBookDatabaseInterface.java
@@ -1,7 +1,23 @@
 package com.group4.comp3350.memorybook.persistence;
 
+import com.group4.comp3350.memorybook.domainobjects.Memory;
+import com.group4.comp3350.memorybook.domainobjects.Story;
+
+import java.util.ArrayList;
+
 /**
  * Created by Bennet on 2/2/2016.
  */
 public interface MemoryBookDatabaseInterface {
+
+    public ArrayList<Memory> getAllMemories();
+    public long addMemory(Memory newMemory);
+    public int deleteMemory(long memoryId);
+
+
+    public ArrayList<Story> getAllStories();
+    public long addStory(Story newStory);
+    public int updateStory(long storyId, Story updatedStory);
+    public int deleteStory(long storyId);
+
 }
