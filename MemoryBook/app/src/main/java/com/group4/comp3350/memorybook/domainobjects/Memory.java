ca84dd4bee509dcd334fda9a1691e5c9479ab729
Chad Kosie <kosie150@gmail.com>
2016-02-02 18:10:18 -0600

    base stuff for memory/story domain objects

--- MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/domainobjects/Memory.java
+++ MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/domainobjects/Memory.java
@@ -4,4 +4,15 @@ package com.group4.comp3350.memorybook.domainobjects;
  * Created by Bennet on 2/2/2016.
  */
 public class Memory {
+
+    private int id;
+    private String title;
+
+    public Memory(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/domainobjects/Story.java
+++ MemoryBook/app/src/main/java/com/group4/comp3350/memorybook/domainobjects/Story.java
@@ -1,7 +1,41 @@
 package com.group4.comp3350.memorybook.domainobjects;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Bennet on 2/2/2016.
  */
 public class Story {
+    private int id = -1;
+    private String title;
+    private HashMap<Integer, Memory> memories;
+
+    public Story(String title) {
+        this.title = title;
+        this.memories = new HashMap<Integer, Memory>();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void add(Memory memory) {
+        this.memories.put(memory.getId(), memory);
+    }
+
+    public void remove(Memory memory) {
+        this.memories.remove(memory.getId());
+    }
+
+    public List<Memory> getMemories() {
+        return new ArrayList<Memory>(memories.values());
+    }
 }
